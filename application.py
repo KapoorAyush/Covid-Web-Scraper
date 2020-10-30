89c03aee02b40ad53023e6062049a075f06e4bce
KapoorAyush <akapoor.gm2000@gmail.com>
2020-10-30 23:23:11 +0530

    mailer added

--- application.py
+++ application.py
@@ -36,17 +36,45 @@ app = Flask(__name__)
 def send_index():
     return send_from_directory('./www', "index.html")
 
+@app.route('/mail', methods=['GET'])
+def send_mail():
+    return send_from_directory('./www', "mail.html")
+
+
 @app.route('/<path:path>', methods=['GET'])
 def send_root(path):
     return send_from_directory('./www', path)
 
 @app.route('/api/scrap', methods=['POST'])
 def scrap():
-
     details=get_corona_detail_of_india()
     response = {"id":str(uuid.uuid4()),"active":details[0],"discharged":details[1],"deaths":details[2]}
     return jsonify(response)
+@app.route('/api/mail', methods=['POST'])
+def mail():
+    import smtplib
+    from email.mime.text import MIMEText
+    from email.mime.multipart import MIMEMultipart
+    subject = request.form.get("subject")
+    sender = request.form.get("email")
+    msg = request.form.get("message")
+    from1=""
+    to=""
 
+    mymsg=MIMEMultipart()
+    mymsg['From']=from1
+    mymsg['To']=to
+    mymsg['Subject']=sender+"**"+subject
+
+    mymsg.attach(MIMEText(msg, 'plain'))
+
+    server=smtplib.SMTP('smtp.gmail.com',587)
+    server.starttls()
+    server.login(from1, "") #password
+    text=mymsg.as_string()
+    server.sendmail(from1, to, text)
+    server.quit()
+    return 'Message Sent'
 
 if __name__ == '__main__':
     app.run(host= '0.0.0.0',debug=True)
